e8b6608689aadf8a9d5bff5b50a05c44cbcc0309
Cristian Alejandro Cisneros Villegas <125778068+AlejandroCisnerosVillegas@users.noreply.github.com>
2024-06-11 14:16:27 -0600

    Add files via upload

--- resultado.php
+++ resultado.php
@@ -1,24 +1,22 @@
 <?php
-
-$num1 = $_POST['numero1'];
-$num2 = $_POST['numero2'];
-$par = '';
-
-if ($num1 == $num2)
-{
-	echo "<script> alert('Los números deben de ser diferentes'); window.location='index.html' </script>";
-}else
-{
-	echo "<div align='center'><h2>Los números pares entre $num1 y $num2 son: <br>";
-	for($num1;$num1<=$num2;$num1++)
+	$num1 = $_POST['numero1'];
+	$num2 = $_POST['numero2'];
+	$par = '';
+	if ($num1 == $num2)
 	{
-		if($num1%2==0)
+		echo "<script> alert('Los números deben de ser diferentes'); window.location='index.html' </script>";
+	}else
+	{
+		echo "<div align='center'><h2>Los números pares entre $num1 y $num2 son: <br>";
+		for($num1;$num1<=$num2;$num1++)
 		{
-			 $par .= $num1 . ',';
+			if($num1%2==0)
+			{
+				$par .= $num1 . ',';
+			}
 		}
+		$par = trim($par, ',');
+		echo $par; 
+		echo "<br><a href='index.html'>Regresar</a></h2></div>";
 	}
-	$par = trim($par, ',');
-	echo $par; 
-	echo "<br><a href='index.html'>Regresar</a></h2></div>";
-}
 ?>
